b6e73b804d5520beb86bc600b9b822aeea4d0664
enjirtmnv <shkensinio@gmail.com>
2021-08-16 17:58:25 +0300

    create component AppBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'fontsource-roboto';
+
+import {AppBar, Container, Toolbar, IconButton, Typography, Box, Button, Paper, Grid} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <AppBar position={'fixed'}>
+        <Container fixed>
+          <Toolbar>
+            <IconButton
+              edge={'start'}
+              color={'inherit'}
+              aria-label={'menu'}
+              className={classes.menuButton}
+            >
+              <MenuIcon/>
+            </IconButton>
+            <Typography
+              variant={'h6'}
+              className={classes.title}
+            >
+              Developer Blog
+            </Typography>
+            <Box mr={3}>
+              <Button color={'inherit'} variant={'outlined'}>Log In</Button>
+            </Box>
+            <Button color={'secondary'} variant={'contained'}>Sign Up</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <main>
+        <Paper
+          className={classes.mainFeaturePost}
+          style={{ backgroundImage: `url(https://source.unsplash.com/random)` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container fixed>
+            <Grid container>
+              <Grid item md={6}>
+
+              </Grid>
+            </Grid>
+          </Container>
+        </Paper>
+      </main>
+    </>
   );
 }
 
